233857505ff4fca4073796ba63d8713ddd4fd996
ThiagoDRangel <info.tec.campos@gmail.com>
2023-10-19 10:31:05 -0300

    fix: class Screen

--- backend/Chess/Program.cs
+++ backend/Chess/Program.cs
@@ -8,12 +8,21 @@ namespace chess_game
     {
         static void Main(string[] args) {
 
-            PositionChess positionChess = new PositionChess('c', 7);
+            try { 
+                Board initialBoard = new Board(8, 8);
 
-            Console.WriteLine(positionChess);
+                initialBoard.setPieceBoard(new Tower(initialBoard, Color.black), new Position(0, 0));
+                initialBoard.setPieceBoard(new Tower(initialBoard, Color.black), new Position(1, 3));
+                initialBoard.setPieceBoard(new King(initialBoard, Color.black), new Position(0, 2));
 
-            Console.WriteLine(positionChess.toPosition());
+                initialBoard.setPieceBoard(new Tower(initialBoard, Color.black), new Position(3, 5));
 
+
+                Screen.printBoard(initialBoard);
+            }
+            catch (BoardException error) {
+                Console.WriteLine(error.Message);
+            } 
             Console.ReadLine();
         }
     }
--- backend/Chess/Screen.cs
+++ backend/Chess/Screen.cs
@@ -9,6 +9,7 @@ namespace chess_game
         {
             for (int i = 0; i < initialBoard.line; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < initialBoard.column; j++)
                 {
                     if (initialBoard.piece(i, j) == null)
@@ -17,11 +18,23 @@ namespace chess_game
                     }
                     else
                     {
-                        Console.Write(initialBoard.piece(i, j) + " ");
-
+                        printPiece(initialBoard.piece(i, j));
+                        Console.Write(' ');
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+        public static void printPiece(Piece piece) {
+            if (piece.color == Color.white) {
+                Console.WriteLine(piece);
+            }
+            else {
+                ConsoleColor playerColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(piece);
+                Console.ForegroundColor = playerColor;
             }
         }
     }
